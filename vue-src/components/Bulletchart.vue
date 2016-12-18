00f883c36669921b10c83f19ac7273c93c62ce44
Kevin <kan.kevin@gmail.com>
2016-12-18 00:56:17 -0500

    Create a separate .vue file for bulletcharts

---
+++ vue-src/components/Bulletchart.vue
@@ -0,0 +1,118 @@
+<template>
+	<div class="bullet-chart-container">
+		<div class="title"><span>{{ titleVal }}</span><span>{{ label }}</span></div>
+		<div class="chart">
+			<div class="ranges">
+				<div v-for="(r, i) in ranges" v-bind:style="{ width: r.width + '%', background: r.colour }"></div>
+			</div>
+			<div v-if="markerPos <= 100" v-bind:style="{ left: 'calc(' + markerPos + '% - 1px)' }" class="marker"></div>
+		</div>
+		<div class="axis">
+			<span>{{ axisTicks[0] }}</span><span>{{ axisTicks[1] }}</span>
+		</div>
+	</div>
+</template>
+
+<script>
+var _ = require("lodash");
+module.exports = {
+	props: ["label", "breakpoints", "point", "isInverted"],
+	computed: {
+		ranges: function() {
+			var self = this;
+			var ranges = [];
+			// Get width
+			self.breakpoints.forEach(function(p, i) {
+				var delta = i === self.breakpoints.length - 1 ? p : p - self.breakpoints[i + 1];
+				ranges.push({ width: 100 * delta / self.breakpoints[0] });
+			});
+			// Get colour
+			var colours = ["#209767", "#59ad85", "#84c2a3", "#add7c2", "#d6ece3"];	// Listed from dark to light
+			colours = colours.slice(0, ranges.length);
+			if (self.isInverted) {
+				colours.reverse();
+			}
+			ranges.forEach(function(r, i) {
+				r.colour = colours[i];
+			});
+			return ranges.reverse();
+		},
+		markerPos: function() {
+			return 100 * this.point / this.breakpoints[0];
+		},
+		axisTicks: function() {
+			var ticks = [0, Math.round(_.max(this.breakpoints))];
+			if (this.label === "mins/game, total") {
+				ticks[1] = Math.round(ticks[1] / 60);
+			}
+			return ticks;
+		},
+		titleVal: function() {
+			if (this.label === "mins/game, total") {
+				return (this.point / 60).toFixed(1);
+			} else {
+				return this.point.toFixed(1);
+			}
+		}
+	}
+};
+</script>
+
+<style>
+.bullet-chart-container {
+	display: inline-block;
+	vertical-align: top;
+	margin: 0 24px 24px 24px;
+	width: 272px;
+	position: relative;
+}
+.bullet-chart-container .chart {
+	position: relative;
+	height: 32px;
+}
+.bullet-chart-container .chart .ranges > div {
+	height: 16px;
+	margin-top: 8px;
+	display: inline-block;
+	vertical-align: top;
+}
+.bullet-chart-container .chart .ranges > div:first-child {
+	border-top-left-radius: 4px;
+	border-bottom-left-radius: 4px;
+}
+.bullet-chart-container .chart .ranges > div:last-child {
+	border-top-right-radius: 4px;
+	border-bottom-right-radius: 4px;
+}
+.bullet-chart-container .chart .marker {
+	position: absolute;
+	height: 100%;
+	width: 2px;
+	background: #121818;
+	top: 0;
+}
+.bullet-chart-container .axis {
+	width: 100%;
+	position: relative;
+}
+.bullet-chart-container .axis span {
+	display: inline-block;
+	vertical-align: top;
+	font-size: 12px;
+	line-height: 16px;
+	width: 50%;
+}
+.bullet-chart-container .axis span:last-child {
+	text-align: right;
+}
+.bullet-chart-container .title {
+	font-size: 22px;
+	font-weight: 700;
+	line-height: 24px;
+}
+.bullet-chart-container .title span:last-child {
+	font-weight: 400;
+	font-size: 14px;
+	margin-left: 6px;
+}
+</style>
